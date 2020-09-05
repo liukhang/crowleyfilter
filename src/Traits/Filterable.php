58a609a8d069da36fb474fa462ca61d01c18fe7d
khangtv <liukhang123@gmail.com>
2020-09-05 16:04:28 +0700

    first commit

---
+++ src/Traits/Filterable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Crowley\Traits;
+
+use Illuminate\Support\Str;
+
+trait Filterable
+{
+    public function scopeFilter($query, $param)
+    {
+        foreach ($param as $field => $value) {
+            $method = 'filter' . Str::studly($field);
+
+            if ($value == '') {
+                continue;
+            }
+
+            if (method_exists($this, $method)) {
+                $this->{$method}($query, $value);
+                continue;
+            }
+
+            if (empty($this->fillable) || !is_array($this->fillable)) {
+                continue;
+            }
+
+            if (in_array($field, $this->fillable)) {
+                $query->where($this->table . '.' . $field, $value);
+                continue;
+            }
+
+            if (key_exists($field, $this->fillable)) {
+                $query->where($this->table . '.' . $this->fillable[$field], $value);
+                continue;
+            }
+        }
+
+        return $query;
+    }
+}
